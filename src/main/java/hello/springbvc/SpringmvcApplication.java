b65728fa81c972c64fb671d5e1905f1828da6ea8
전성훈 <shajh9708@naver.com>
2023-08-01 00:28:56 +0900

    springmvc 기본 기능 복습

--- src/main/java/hello/springbvc/SpringmvcApplication.java
+++ src/main/java/hello/springbvc/SpringmvcApplication.java
@@ -1,11 +1,11 @@
-package hello.springmvc;
+package hello.springbvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringmvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcApplication.class, args);
+    }
 }
